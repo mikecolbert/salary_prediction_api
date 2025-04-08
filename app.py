184e38ab9d46563911b13bafb3da46ec2aae81c6
mikecolbert <michael-colbert@uiowa.edu>
2025-04-08 11:47:05 -0500

    Updated Flask and Flask-CORS, removed unnecessary files

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ def home():
     return "<h1>Salary Prediction API</h1><p>BAIS:3300 - Digital Product Development</p><p>Mike Colbert</p>"
 
 
-# predict route
+# /predict route
 @app.route("/predict", methods=["GET"])
 def predict():
     return "<h1>Prediction route is working... </h1>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002, debug=True)
